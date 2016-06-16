badc40995374dbf6128435e72395c6acd290d196
biss2016 <oesteban@stanford.edu>
2016-06-16 10:39:11 +0200

    Add case: missing noise mask

--- snr.py
+++ snr.py
@@ -1,6 +1,9 @@
 
-
-def snr(img, smask, nmask):
+def snr(img, smask, nmask=None):
     fg_mean = img[smask > 0].mean()
+
+    if nmask is None:
+        nmask = smask
+
     bg_std = img[nmask > 0].std()
     return float(fg_mean / bg_std)
